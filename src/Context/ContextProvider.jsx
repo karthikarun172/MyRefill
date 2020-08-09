269f896136fbc85056287b88f62f458cb7d83e25
arunkarthik132 <karthikarun172@gmail.com>
2020-08-09 17:44:39 +0530

    second commit

---
+++ src/Context/ContextProvider.jsx
@@ -0,0 +1,20 @@
+import React, { useReducer } from "react";
+
+export default (reducer, actions, intialState) => {
+  const Context = React.createContext();
+
+  const Provider = ({ children }) => {
+    const [state, dispatch] = useReducer(reducer, intialState);
+
+    const boundAction = {};
+    for (let key in actions) {
+      BoundAction[key] = actions[key](dispatch);
+    }
+    return (
+      <Context.Provider value={{ state, ...boundAction }}>
+        {children}
+      </Context.Provider>
+    );
+  };
+  return { Context, Provider };
+};

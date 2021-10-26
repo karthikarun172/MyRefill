67ffdfd8f143269a9260b5dbc5a363aab3dff521
karthikarun172 <karthikarun172@gmail.com>
2021-10-26 11:51:10 +0530

    first commit

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+/** @format */
+
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
@@ -13,7 +15,6 @@ import { NavigationContainer } from "@react-navigation/native";
 import { Provider as RegisterProvider } from "./src/Context/RegisterContext";
 import { Provider as CityProvider } from "./src/Context/CityContext";
 import { Provider as StationProvider } from "./src/Context/StationContext";
-
 
 import Navigation from "./src/Navigations/Navigation";
 
@@ -37,5 +38,6 @@ const styles = StyleSheet.create({
     backgroundColor: "#00A2F9",
     alignItems: "center",
     justifyContent: "center",
+    flexDirection: "column",
   },
 });
